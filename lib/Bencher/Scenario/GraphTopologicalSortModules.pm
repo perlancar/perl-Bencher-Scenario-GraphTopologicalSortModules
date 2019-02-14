d7f64ecbabad56698753f1f621833f488239cc81
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-15 00:28:53 +0700

    Add more datasets: 10nodes-35edges, 100nodes-100edges, 100nodes-500edges

--- lib/Bencher/Scenario/GraphTopologicalSortModules.pm
+++ lib/Bencher/Scenario/GraphTopologicalSortModules.pm
@@ -38,7 +38,19 @@ our $scenario = {
         },
 
         {
-            name => 'g6',
+            name => '2nodes-1edge',
+            args => {
+                graph => {
+                    a => ['b'],
+                    b => [], # Algorithm::Dependency requires all nodes to be specified
+                },
+                unsorted => ['b','a'],
+            },
+            result => ['a', 'b'],
+        },
+
+        {
+            name => '6nodes-7edges',
             args => {
                 graph => {
                     'a' => [ 'b', 'c' ],
@@ -51,6 +63,42 @@ our $scenario = {
                 unsorted => ['z', 'a', 'x', 'c', 'b', 'y'],
             },
             result => ['a', 'b', 'c', 'x', 'y', 'z'],
+        },
+
+        {
+            name => '10nodes-35edges',
+            args => {
+                graph => {
+                    (map { sprintf("%02d",$_) => [grep {$_<=10} sprintf("%02d", $_+1), sprintf("%02d", $_+2), sprintf("%02d", $_+3), sprintf("%02d", $_+4), sprintf("%02d", $_+5)] } 1..10)
+                },
+                unsorted => [reverse(map {sprintf("%02d", $_)} 1..10)],
+            },
+            result => [map {sprintf("%02d", $_)} 1..10],
+        },
+
+        {
+            # Algorithm::Dependency gives different results when we use 1..100 instead of 001 .. 100
+            name => '100nodes-100edges',
+            args => {
+                graph => {
+                    (map { (sprintf("%03d",$_) => [$_==1 ? ("002","003") : $_==100 ? () : (sprintf("%03d", $_+1))]) } 1..100)
+                },
+                unsorted => [reverse(map {sprintf("%03d", $_)} 1..100)],
+            },
+            result => [map {sprintf("%03d", $_)} 1..100],
+        },
+
+        {
+            name => '100nodes-500edges',
+            args => {
+                graph => {
+                    (map { (sprintf("%03d",$_) => [$_==1 ? ("002".."021") : (grep {$_<=100} sprintf("%03d", $_+1), sprintf("%03d", $_+2), sprintf("%03d", $_+3), sprintf("%03d", $_+4), sprintf("%03d", $_+5))]) } 1..100)
+                },
+                unsorted => [reverse(map {sprintf("%03d", $_)} 1..100)],
+            },
+            result => [map {sprintf("%03d", $_)} 1..100],
+            # Sort::Topological eats too much memory
+            include_by_default => 0,
         },
 
         # cyclic datasets not included by default because they hang
